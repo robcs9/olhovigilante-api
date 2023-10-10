cf0d639afa06e4adb4ac4667c049f107ffa4d575
robcs9 <rcs9@proton.me>
2023-10-10 08:50:31 -0300

    Organização inicial do backend

---
+++ src/main/java/br/edu/ifpe/olhovigilanteapi/OlhovigilanteApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.olhovigilanteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlhovigilanteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlhovigilanteApiApplication.class, args);
+	}
+
+}
