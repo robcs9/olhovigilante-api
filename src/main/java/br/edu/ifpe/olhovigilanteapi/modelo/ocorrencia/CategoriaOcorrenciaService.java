d2fda0865746c935340e92af9f4d742e6da50ae7
robcs9 <rcs9@protonmail.com>
2023-11-22 15:22:27 -0300

    Ajustes no Service de categoria de ocorrencia

--- src/main/java/br/edu/ifpe/olhovigilanteapi/modelo/ocorrencia/CategoriaOcorrenciaService.java
+++ src/main/java/br/edu/ifpe/olhovigilanteapi/modelo/ocorrencia/CategoriaOcorrenciaService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoriaOcorrenciaService {
     
     @Autowired
